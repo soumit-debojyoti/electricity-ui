773ef7a35915c69d1f0ee7015f1462781926525d
soumit-anthonio <nag.soumit@gmil.com>
2019-08-10 20:49:45 +0530

    add

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -83,3 +83,5 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /** Evergreen browsers require these. **/
 import 'core-js/es/reflect';
 // import 'core-js/es7/reflect';
+import 'hammerjs';
+import 'web-animations-js';
