34cc29196473045cca717f685e8ec15e2a9e54d1
DESKTOP-ELII2RM\deboj <debojyoti1112@gmail.com>
2019-09-07 14:25:47 +0530

    changes

--- src/app/recharge/recharge.component.ts
+++ src/app/recharge/recharge.component.ts
@@ -4,6 +4,7 @@ import { AlertService } from '../services/common.service/alert.service';
 import { LoadingScreenService } from '../services/loading-screen/loading-screen.service';
 import { RechargeAPI } from '../models/common.model';
 import { LOCAL_STORAGE, WebStorageService } from 'angular-webstorage-service';
+import { UserService } from '../services/user.service/user.service';
 @Component({
   selector: 'app-recharge',
   templateUrl: './recharge.component.html',
@@ -16,9 +17,12 @@ export class RechargeComponent implements OnInit {
   public operatorName: string;
   public mobileNumber: number;
   public rechargeAmount: number;
+  public customerName: string;
+  public customerMobile: string;
+  public insufficientBalance: boolean = false;
   constructor(private common: CommonService, private alertService: AlertService,
     private loadingScreenService: LoadingScreenService,
-    @Inject(LOCAL_STORAGE) private storage: WebStorageService) { }
+    @Inject(LOCAL_STORAGE) private storage: WebStorageService, private userService: UserService) { }
 
   ngOnInit() {
     this.rechargeTypes = ['PREPAID', 'DTH', 'POSTPAID'];
@@ -34,21 +38,32 @@ export class RechargeComponent implements OnInit {
   }
 
   recharge(): void {
-    let rechargeAPI = this.apiInfoList.find( x => x.operatorName === this.operatorName);
+    const rechargeAPI = this.apiInfoList.find( x => x.operatorName === this.operatorName);
     const userID = this.storage.get('user_id');
-    console.log(userID, 'user id');
-    console.log("recharge called!");
+    debugger;
     this.common.insertTransaction(this.rechargeMode, userID, this.rechargeAmount.toString()).subscribe(
       (response: number) => {
         rechargeAPI.apiValue = rechargeAPI.apiValue.replace('#Mobile#', this.mobileNumber.toString());
         rechargeAPI.apiValue = rechargeAPI.apiValue.replace('#Amount#', this.rechargeAmount.toString());
         rechargeAPI.apiValue = rechargeAPI.apiValue.replace('#Order#', response.toString());
         console.log(rechargeAPI.apiValue );
-        this.common.recharge(rechargeAPI.apiValue).subscribe( (response: any) => {
-          console.log(response);
+        this.common.recharge(rechargeAPI.apiValue).subscribe( (innerResponse: any) => {
+          console.log('Recharge Sub called');
+          console.log(innerResponse);
         });
       }
     );
+  }
+
+  checkAndDeductBalance(): void {
+    const userID = this.storage.get('user_id');
+    this.userService.getWalletBalance(userID).subscribe( (response: any) => {
+      if (+response.walletBalance >= this.rechargeAmount) {
+        this.recharge();
+      } else {
+        this.insufficientBalance = true;
+      }
+    });
   }
 
 }
