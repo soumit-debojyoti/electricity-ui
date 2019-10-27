acc33146cb5c4d4afc55fa0eb361370eeda39a32
soumit-anthonio <nag.soumit@gmil.com>
2019-10-28 02:02:08 +0530

    added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://nagsoumit-001-site1.itempurl.com/api/', // 'http://localhost:53019/',
-  apiUrl: 'http://nagsoumit-001-site1.itempurl.com/api/api/', // 'http://localhost:53019/api/',
+  baseUrl: 'http://nagsoumit-001-site1.itempurl.com/api_url/', // 'http://localhost:53019/',
+  apiUrl: 'http://nagsoumit-001-site1.itempurl.com/api_url/api/', // 'http://localhost:53019/api/',
 };
